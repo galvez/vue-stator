90d6550714b4cf11a9e832552fd3cd1095fcf228
Jonas Galvez <jonasgalvez@gmail.com>
2019-10-11 18:44:39 -0300

    comments

--- examples/spa/nuxt.config.js
+++ examples/spa/nuxt.config.js
@@ -1,4 +1,17 @@
+import { resolve } from 'path'
+
 export default {
   mode: 'spa',
-  plugins: ['~/plugins/vue-stator']
+  plugins: ['~/plugins/vue-stator'],
+  // For library testing only:
+  build: {
+    transpile: ['vue-stator'],
+    extend (config) {
+      config.resolve.alias['vue-stator'] = resolve(__dirname, '..', '..')
+    }
+  }
 }
+
+
+// Note: the `vue-stator/nuxt` module supports SPA mode.
+// Usage of Vue.use() here (in plugins/vue-stator) is purely didactic
