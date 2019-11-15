b72b62e583b883fcb87d4858615ecd0cfc267884
pimlie <pimlie@hotmail.com>
2019-11-15 18:04:29 +0100

    feat: use nuxt vuex module for stator initialization

--- nuxt/index.js
+++ nuxt/index.js
@@ -1,75 +1,13 @@
-import fs from 'fs'
 import path from 'path'
-import { promisify } from 'util'
-import consola from 'consola'
 
-const readDir = promisify(fs.readdir)
-const logger = consola.withScope('vue-stator')
-
-function exists (p) {
-  return new Promise((resolve, reject) => {
-    fs.access(p, fs.constants.F_OK, (err) => {
-      if (err) {
-        resolve(false)
-        return
-      }
-
-      resolve(true)
-    })
-  })
-}
-
-async function loadStatorModules (baseDir) {
-  // TODO: handle error
-  const files = await readDir(baseDir)
-
-  const modules = await Promise.all(files
-    .filter(modulePath => !(['actions.js', 'state.js', 'getters.js', 'index.js'].includes(modulePath)))
-    .map(async (modulePath) => {
-      const { name: namespace } = path.parse(modulePath)
-
-      if (modulePath.endsWith('.js')) {
-        return {
-          namespace,
-          actions: modulePath
-        }
-      }
-
-      let statorModule
-      for (const type of ['actions', 'getters']) {
-        if (await exists(path.join(baseDir, modulePath, `${type}.js`))) {
-          statorModule = statorModule || { namespace }
-          statorModule[type] = path.join(modulePath, `${type}.js`)
-        }
-      }
-
-      return statorModule
-    }))
-
-  return [modules.filter(Boolean), files]
-}
-
-export default async function NuxtStatorModule (options) {
+export default function NuxtStatorModule (options) {
   options = {
     ...options,
     ...this.options.stator
   }
 
+  const statorDir = options.baseDir || 'store'
   const disableStore = 'disableVuex' in options ? options.disableVuex : true
-
-  const baseDirName = options.baseDir || 'store'
-  const baseDir = path.join(this.options.srcDir, baseDirName)
-
-  if (!await exists(baseDir)) {
-    logger.warn(`Folder '~/${path.relative(this.nuxt.options.srcDir, baseDir)}' does not exists, vue-stator will not be enabled`)
-    return
-  }
-
-  const [statorModules, files] = await loadStatorModules(baseDir)
-
-  const hasState = files.includes('state.js')
-  const hasGlobalActions = files.includes('actions.js')
-  const hasGlobalGetters = files.includes('getters.js')
 
   // Disable default Vuex store (options.features only exists in Nuxt v2.10+)
   if (this.options.features && disableStore) {
@@ -78,16 +16,25 @@ export default async function NuxtStatorModule (options) {
 
   this.addPlugin({
     src: path.resolve(__dirname, 'plugin.js'),
-    fileName: 'vue-stator.js',
-    options: {
-      isSPA: this.options.mode === 'spa',
-      baseDir: baseDirName,
-      localStorage: options.localStorage,
-      sessionStorage: options.sessionStorage,
-      statorModules,
-      hasState,
-      hasGlobalActions,
-      hasGlobalGetters
-    }
+    fileName: 'vue-stator.js'
+  })
+
+  let statorModules
+  this.nuxt.hook('builder:prepared', async (builder) => {
+    statorModules = (await builder.resolveRelative(statorDir))
+      .sort(({ src: p1 }, { src: p2 }) => {
+        // modules are sorted from low to high priority (for overwriting properties)
+        let res = p1.split('/').length - p2.split('/').length
+        if (res === 0 && p1.includes('/index.')) {
+          res = -1
+        } else if (res === 0 && p2.includes('/index.')) {
+          res = 1
+        }
+        return res
+      })
+  })
+
+  this.nuxt.hook('build:templates', ({ templateVars }) => {
+    templateVars.statorModules = statorModules
   })
 }
