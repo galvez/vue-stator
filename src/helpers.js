d066f6ea88c9b60eec11eecfba1bbb9d54301d7a
pimlie <pimlie@hotmail.com>
2019-11-15 15:40:12 +0100

    feat: support map aliases

--- src/helpers.js
+++ src/helpers.js
@@ -15,48 +15,54 @@ export function $delete (...args) {
   Vue.delete(...args)
 }
 
-export function mapState (namespacedProperties) {
+export function map (properties, assignMapping) {
   const mapped = {}
 
-  for (const prop of ensureArray(namespacedProperties)) {
-    mapped[prop] = {
-      get () {
-        return getPropertyByNamespace(this.$state, prop)
-      },
-      set (value) {
-        return setPropertyByNamespace(this.$state, prop, value)
-      }
+  if (typeof properties !== 'object') {
+    properties = ensureArray(properties)
+  }
+
+  if (Array.isArray(properties)) {
+    for (const prop of properties) {
+      mapped[prop] = assignMapping(prop)
     }
+
+    return mapped
+  }
+
+  for (const propName in properties) {
+    mapped[propName] = assignMapping(properties[propName])
   }
 
   return mapped
+}
+
+export function mapState (namespacedProperties) {
+  return map(namespacedProperties, prop => ({
+    get () {
+      return getPropertyByNamespace(this.$state, prop)
+    },
+    set (value) {
+      return setPropertyByNamespace(this.$state, prop, value)
+    }
+  }))
 }
 
 export function mapGetters (namespacedProperties) {
-  const mapped = {}
-
-  for (const prop of ensureArray(namespacedProperties)) {
-    mapped[prop] = function (...args) {
+  return map(namespacedProperties, (prop) => {
+    return function (...args) {
       return getPropertyByNamespace(this.$getters, prop)
     }
-  }
-
-  return mapped
+  })
 }
 
 export function mapActions (namespacedProperties) {
-  const mapped = {}
-
-  for (const prop of ensureArray(namespacedProperties)) {
-    mapped[prop] = function (...args) {
+  return map(namespacedProperties, (prop) => {
+    return function (...args) {
       const action = getPropertyByNamespace(this.$actions, prop)
       if (action) {
         return action(...args)
       }
-
-      // TODO: warn?
     }
-  }
-
-  return mapped
+  })
 }
